1f011f2266c07fcd92933d7be19e40a99023bf92
Jake Zegil <jake.zegil@mail.utoronto.ca>
2021-07-15 09:50:03 -0400

    UPDATE styling tooltip, promptbutton

--- src/components/ToolTip/ToolTip.js
+++ src/components/ToolTip/ToolTip.js
@@ -1,20 +1,11 @@
 import React from "react";
 
-import styles from "./ToolTip.css"
+import styles from "./ToolTip.css";
 
 const ToolTip = ({ children, tooltip, tooltipStyles, containerStyles }) => {
   return (
-    <div
-      className={styles.tooltipContainer}
-      style={{ position: "relative", ...containerStyles }}
-    >
-      <div
-        className={styles.tooltip}
-        style={{
-         
-          ...tooltipStyles,
-        }}
-      >
+    <div className={styles.tooltipContainer} style={{ ...containerStyles }}>
+      <div className={styles.tooltip} style={{ ...tooltipStyles }}>
         {tooltip}
       </div>
       {children}
