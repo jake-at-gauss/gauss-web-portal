391be841becf8b5fbbf6609eb332cd2375c9a790
Jake Zegil <jake.zegil@mail.utoronto.ca>
2021-07-30 09:59:15 -0400

    FIX email signup regex

--- src/utils/verification.js
+++ src/utils/verification.js
@@ -1,7 +1,8 @@
 //File for any form verification
 
 import { EMAIL, EMAIL_NOT_VALID, UNFILLED } from "../constants";
-const emailRegex = /[^\\.\\s@:](?:[^\\s@:]*[^\\s@:\\.])?@[^\\.\\s@]+(?:\\.[^\\.\\s@]+)*/;
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const verifyFormFields = (formFields) => {
   //function for returning specific errors relating to the form fields
@@ -11,7 +12,7 @@ export const verifyFormFields = (formFields) => {
     } else {
       switch (key) {
         case EMAIL:
-          return !emailRegex.test(value) && EMAIL_NOT_VALID;
+          return !emailRegex.test(value.toLowerCase()) && EMAIL_NOT_VALID;
         default:
           return false;
       }
